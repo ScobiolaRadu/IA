32f517b55c875a351cf6124c2eb7e1f7c31519c2
ScobiolaRadu <80578568+ScobiolaRadu@users.noreply.github.com>
2023-04-10 17:45:46 +0300

    Balanced the data set
    First successful model

--- ml/main.py
+++ ml/main.py
@@ -5,6 +5,8 @@ from tensorflow.keras import layers
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 from glob import glob
+from sklearn.utils import class_weight
+import tensorflow.keras.callbacks as callbacks
 
 train_labels = pd.read_csv("data/train_labels.txt", delimiter=",", header=None, names=["id", "class"], skiprows=1)
 val_labels = pd.read_csv("data/validation_labels.txt", delimiter=",", header=None, names=["id", "class"], skiprows=1)
@@ -16,14 +18,22 @@ val_labels['id'] = val_labels['id'].apply(lambda x: '{0:0>6}.png'.format(x))
 train_labels["class"] = train_labels["class"].astype(str)
 val_labels["class"] = val_labels["class"].astype(str)
 
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=20,
+                                   horizontal_flip=True,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   )
 
-# Create image data generators for the training, validation, and test sets
-train_datagen = ImageDataGenerator(rescale=1./255)
 val_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 test_dir = "data/data/"
 test_ids = range(17001, 22150)
+
+class_weights = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(train_labels['class']), y=train_labels['class'])
+class_weights_dict = dict(enumerate(class_weights))
+
 
 train_generator = train_datagen.flow_from_dataframe(
     train_labels,
@@ -32,7 +42,9 @@ train_generator = train_datagen.flow_from_dataframe(
     y_col="class",
     target_size=(224, 224),
     batch_size=32,
-    class_mode="binary")
+    class_mode="binary",
+    #sample_weight=class_weights_dict[train_labels['class'].values]
+    )
 
 val_generator = val_datagen.flow_from_dataframe(
     val_labels,
@@ -53,11 +65,8 @@ test_generator = test_datagen.flow_from_dataframe(
     class_mode=None,
     shuffle=False)
 
-# Define the CNN architecture
-# Define the CNN architecture
 model = tf.keras.Sequential([
-    layers.experimental.preprocessing.Rescaling(scale=1./255),
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)),
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
@@ -68,24 +77,23 @@ model = tf.keras.Sequential([
     layers.Dense(1, activation='sigmoid')
 ])
 
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
-# Compile the model
-model.compile(optimizer='adam',
+model.compile(optimizer=optimizer,
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-# Train the model
 history = model.fit(train_generator,
                     steps_per_epoch=train_generator.samples//train_generator.batch_size,
-                    epochs=1,
+                    epochs=2,
                     validation_data=val_generator,
-                    validation_steps=val_generator.samples//val_generator.batch_size)
+                    validation_steps=val_generator.samples//val_generator.batch_size,
+                    class_weight=class_weights_dict,)
 
-# Evaluate the model on the validation set
+
 model.evaluate(val_generator)
 
-# Make predictions on the test set and output the results in the desired format
 preds = model.predict(test_generator)
 preds = np.where(preds > 0.5, 1, 0)
 output_df = pd.DataFrame({'id': range(17001, 22150), 'class': preds.flatten()})
-output_df.to_csv("data/submission.csv", sep=',', index=False, header=False)
+output_df.to_csv("data/submission.csv", sep=',', index=False, header=True)
